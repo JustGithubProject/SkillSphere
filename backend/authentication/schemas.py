6665ae41368e7f47ac9abb610678e62906d4d730
Staid123 <pavlenkokirill120@gmail.com>
2024-09-04 16:44:13 +0300

    Added UserBase

--- backend/authentication/schemas.py
+++ backend/authentication/schemas.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, EmailStr
 
+
+class UserBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True, strict=True)
+
+    username: str
+    email: EmailStr
+    password_hash: str
+    first_name: str
+    last_name: str
+    
 
 class TokenSchema(BaseModel):
     access_token: str
