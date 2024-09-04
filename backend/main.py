d30ad2711f95cd7bf7737b33489ee29148623359
Staid123 <pavlenkokirill120@gmail.com>
2024-09-04 16:43:36 +0300

    new router for courses

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI 
+from course.router import router as course_router
 
 
 # Initialize FastAPI application
 app = FastAPI(
     title="SkillSphere API"
 )
+
+app.include_router(course_router)
 
 if __name__ == "__main__":
     import uvicorn
