875aa3a3cd2fc15dd078d86716a38cb0deb4dbff
Staid123 <pavlenkokirill120@gmail.com>
2024-09-04 14:56:14 +0300

    New tables Course, User and CourseInstructor

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,13 +1,22 @@
 from datetime import datetime
-from sqlalchemy import TIMESTAMP, Boolean, LargeBinary, UniqueConstraint, func
+from sqlalchemy import (
+    TIMESTAMP, 
+    Boolean, 
+    ForeignKey, 
+    LargeBinary, 
+    String, 
+    UniqueConstraint, 
+    func
+)
 from sqlalchemy.orm import (
     Mapped,
     DeclarativeBase,
     declared_attr,
     mapped_column,
+    relationship,
 )
 
-from enums import Role
+from enums import CourseLevel, Role
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -20,7 +29,6 @@ class Base(DeclarativeBase):
     
 
 class User(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str]
     password_hash: Mapped[bytes] = mapped_column(LargeBinary)
@@ -31,6 +39,33 @@ class User(Base):
     last_login: Mapped[datetime]
     active: Mapped[bool] = mapped_column(Boolean, default=True, server_default='true')
 
+    courses_created = relationship('Course', back_populates='creator')
+    courses_instructed = relationship('Course', secondary='course_instructors', back_populates='instructors')
+
     __table_args__ = (
         UniqueConstraint("first_name", "last_name"),
     )
+
+class Course(Base):
+    title: Mapped[str] = mapped_column(String(50), unique=True)
+    description: Mapped[str]
+    price: Mapped[int]
+    level: Mapped["CourseLevel"] = mapped_column(default=CourseLevel.BEGINNER)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
+
+    creator_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    creator = relationship('User', back_populates='courses_created')
+    instructors = relationship('User', secondary='course_instructors', back_populates='courses_instructed')
+
+
+# Таблица связи курсов и инструкторов
+class CourseInstructor(Base):
+    __tablename__ = 'course_instructors'
+    
+    course_id: Mapped[int] = ForeignKey('courses.id')
+    instructor_id: Mapped[int] = ForeignKey('users.id')
+    
+    # Указываем обратные связи (не обязательно, но полезно)
+    course = relationship('Course', back_populates='instructors')
+    instructor = relationship('User', back_populates='courses_instructed')
