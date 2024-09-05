c55b19acbf558bab0a65a1f406634fba06d1423f
Kirill <pavlenkokirill120@gmail.com>
2024-09-05 22:00:48 +0300

    Added patch and delete endpoints

--- backend/course/router.py
+++ backend/course/router.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Query, status
 from services.course_service import CourseService, get_course_service
 from enums import CourseLevel
-from course.schemas import CourseInput, CourseOutput
+from course.schemas import CourseInput, CourseOutput, CourseUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import session_getter
 
@@ -50,6 +50,30 @@ async def get_course_by_id(
     course_id: int
 ) -> CourseOutput:
     return await course_service.get_course_by_id(
+        session=session,
+        course_id=course_id
+    )
+
+@router.patch("/{course_id}/", response_model=CourseOutput)
+async def update_course(
+    course_service: Annotated[CourseService, Depends(get_course_service)],
+    session: Annotated[AsyncSession, Depends(session_getter)],
+    course_update: CourseUpdate,
+    course_id: int,
+) -> CourseOutput:
+    return await course_service.update_course(
+        session=session,
+        course_update=course_update,
+        course_id=course_id
+    )
+
+@router.delete("/{course_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_course(
+    course_service: Annotated[CourseService, Depends(get_course_service)],
+    session: Annotated[AsyncSession, Depends(session_getter)],
+    course_id: int,
+) -> None:
+    return await course_service.delete_course(
         session=session,
         course_id=course_id
     )
