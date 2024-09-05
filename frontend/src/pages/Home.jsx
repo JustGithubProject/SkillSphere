a24976adca83a58ee882a2d97e392ebc202168e8
JustGithubProject <Kropivadanil@gmail.com>
2024-09-05 14:59:43 +0300

    Added basic content of home page without styles yet

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+import IntroSection from '../components/IntroSection/IntroSection';
+import CoursesSection from '../components/CoursesSection/CoursesSection';
+import ProgramsSection from '../components/ProgramsSection/ProgramsSection';
+import TeachersSection from '../components/TeachersSection/TeachersSection';
+
 
 const Home = () => {
     return (
         <div>
             <Header/>
+            <IntroSection/>
+            <CoursesSection/>
+            <ProgramsSection/>
+            <TeachersSection/>
+            <Footer/>
         </div>
     );
 };
