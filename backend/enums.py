a3987117d264a44bbac6985d61a9d60039c2952c
Staid123 <pavlenkokirill120@gmail.com>
2024-09-06 21:28:01 +0300

    Deleted Role class

--- backend/enums.py
+++ backend/enums.py
@@ -1,11 +1,6 @@
 from enum import Enum
 
 
-class Role(Enum):
-    USER = "user"
-    ADMIN = "admin"
-    GUEST = "guest"
-
 class CourseLevel(Enum):
     BEGINNER = "Beginner"
     INTERMEDIATE = "Intermediate"
