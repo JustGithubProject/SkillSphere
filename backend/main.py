7a94d0b37a0485af7ebe969532709ecba23e4831
Staid123 <pavlenkokirill120@gmail.com>
2024-09-06 21:28:06 +0300

    new router

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI 
 from course.router import router as course_router
+from authentication.routers import router as authentication_router
 
 
 # Initialize FastAPI application
 app = FastAPI(
-    title="SkillSphere API"
+    title="SkillSphere API",
 )
 
 app.include_router(
     router=course_router,
+    prefix="/api/v1"
+)
+
+app.include_router(
+    router=authentication_router,
     prefix="/api/v1"
 )
 
