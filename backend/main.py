b4d93f4f02882ce5c8425fc0a49b64a4e19b3e71
JustGithubProject <Kropivadanil@gmail.com>
2024-09-07 20:32:27 +0300

    Set up CORS

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from course.router import router as course_router
 from authentication.routers import router as authentication_router
 
@@ -16,6 +18,23 @@ app.include_router(
 app.include_router(
     router=authentication_router,
     prefix="/api/v1"
+)
+
+# Allowed origins 
+origins = [
+    "http://172.18.0.4:3000",
+    "http://0.0.0.0:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+]
+
+# To allow access from frontend to backend routers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 if __name__ == "__main__":
