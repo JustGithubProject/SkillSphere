99cecb87c8bde61e49f471bbed91684c0e0fd7d7
Kirill <pavlenkokirill120@gmail.com>
2024-09-09 13:43:01 +0300

    Post endpoint for module router

--- backend/module/router.py
+++ backend/module/router.py
@@ -1,11 +1,11 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from authentication.schemas import UserOut
 from authentication.validation import get_current_active_auth_user
 from services.module_service import ModuleService, get_module_service
 from database.database import session_getter
-from module.schemas import ModuleOutput
+from module.schemas import ModuleOutput, ModuleInput
 
 
 router = APIRouter(
@@ -25,4 +25,17 @@ async def get_all_modules_by_course_id(
         session=session,
         user=user,
         course_id=course_id
+    )
+
+@router.post("/", response_model=ModuleOutput, status_code=status.HTTP_201_CREATED)
+async def create_module_in_course(
+    module_service: Annotated[ModuleService, Depends(get_module_service)],
+    session: Annotated[AsyncSession, Depends(session_getter)],
+    user: Annotated[UserOut, Depends(get_current_active_auth_user)],
+    module_input: ModuleInput
+) -> ModuleOutput:
+    return await module_service.create_module(
+        session=session,
+        user=user,
+        module_input=module_input
     )
