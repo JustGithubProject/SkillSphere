413c97cfea42b601d45ae28b7a050ea86ab57bd3
Kirill <pavlenkokirill120@gmail.com>
2024-09-12 09:31:52 +0300

    comment service

--- backend/services/comment_service.py
+++ backend/services/comment_service.py
@@ -1,12 +1,14 @@
+from sqlalchemy import select
 from authentication.schemas import UserOut
 from repositories.comment_repository import CommentRepository
-from comment.schemas import CommentInput, CommentOutput
+from comment.schemas import CommentInput, CommentOutput, CommentUpdate
 from course.schemas import CourseOutput
 from services.course_service import CourseService, get_course_service
 from repositories.course_repository import CourseRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.models import Comment
 from authentication.custom_exceptions import not_enough_rights_exception
+from sqlalchemy.orm import selectinload
 
 
 class CommentService:
@@ -25,12 +27,12 @@ class CommentService:
         session: AsyncSession,
         course_id: int
     ) -> list[CommentOutput]:
-        course: CourseOutput = await self.course_service.get_course_by_id_no_auth(
+        comments: list[Comment] = await self.comment_repository.get_comments_by_course_id(
             session=session,
             course_id=course_id
         )
-        return course.comments
-
+        return [CommentOutput.model_validate(comment, from_attributes=True) for comment in comments]
+                
     async def create_comment(
         self,
         session: AsyncSession,
@@ -45,7 +47,32 @@ class CommentService:
             comment_input=comment_input
         )
         return CommentOutput.model_validate(comment, from_attributes=True)
-        
+    
+    async def update_comment(
+        self,
+        session: AsyncSession,
+        comment_id: int,
+        user: UserOut,
+        comment_update: CommentUpdate
+    ) -> None:
+        return await self.comment_repository.update_comment(
+            session=session,
+            comment_update=comment_update,
+            comment_id=comment_id,
+            user=user
+        )
+
+    async def delete_comment(
+        self,
+        session: AsyncSession,
+        comment_id: int,
+        user: UserOut,
+    ) -> None:
+        return await self.comment_repository.delete_comment(
+            session=session,
+            comment_id=comment_id,
+            user=user
+        )
 
 def get_comment_service():
     return CommentService(CommentRepository(), CourseRepository())
