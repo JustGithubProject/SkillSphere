04d210b7ef4ce31e50e828946e7bdee1acfd8d45
JustGithubProject <Kropivadanil@gmail.com>
2024-09-30 15:02:48 +0300

    Added protocol to BASE_URL and changed test a little bit

--- tests/backend/course/test_get_all_courses_no_auth.py
+++ tests/backend/course/test_get_all_courses_no_auth.py
@@ -1,18 +1,13 @@
-import pytest
-
-from fastapi.testclient import TestClient
-
-from backend.main import app
+import httpx
 
 
-client = TestClient(app)
-
-BACKEND_BASE_URL = "127.0.0.1:8000"
+BACKEND_BASE_URL = "http://127.0.0.1:8000"
 
 
 def test_fetch_all_courses_no_auth():
-    response = client.get(f"{BACKEND_BASE_URL}/api/v1/all/no-auth/")
-    data = response.json()
-    assert 200 <= response.status_code < 300
-    assert isinstance(data, list)
+    with httpx.Client() as client:
+        response = client.get(f"{BACKEND_BASE_URL}/api/v1/all/no-auth")
+        data = response.json()
+        assert 200 <= response.status_code < 300
+        assert isinstance(data, list)
     
