d8d172f2c93d47a079955d45967fa771c7f88e4c
JustGithubProject <Kropivadanil@gmail.com>
2024-10-12 14:21:38 +0300

    Started to do forgot password

---
+++ frontend/src/components/ForgotPasswordForm/ForgotPasswordFormEN.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+const ForgotPasswordFormEN = () => {
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const handleForgotPasswordForm = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        setMessage('');
+
+        try {
+            await axios.post(
+                `http://127.0.0.1:8000/api/v1/jwt/auth/forgot/password/?email=${email}`
+            );
+            setMessage("Check your email for the reset link");
+        } catch(error) {
+            if (error.response) {
+                setMessage('Error: ' + 'Something went Wrong');
+            } else {
+                setMessage('Network error. Please try again later.');
+            }
+        } finally {
+            setLoading(false);
+        }
+
+
+    }
+
+    return (
+        <form onSumbit={handleForgotPasswordForm} className="form-box">
+            <h3 className="h4 text-black mb-4">Change password</h3>
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="form-group">
+            <button type="submit" disabled={loading} className="btn btn-primary btn-pill">
+                {loading ? 'Sending...' : 'Send Reset Link'}
+            </button>
+            {message && <p>{message}</p>}
+            </div>
+        </form>
+    );
+};
+
+export default ForgotPasswordFormEN;
