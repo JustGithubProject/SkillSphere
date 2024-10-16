dde5e6da7d27564527b7c4664d28807775cfb793
JustGithubProject <Kropivadanil@gmail.com>
2024-10-16 17:32:29 +0300

    Added Sidebar

--- frontend/src/components/EditCourseComponents/Navbar.jsx
+++ frontend/src/components/EditCourseComponents/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Logo from './Logo';
 import NavLinks from './NavLinks';
-import SearchBar from './SearchBar';
 import styles from './Navbar.module.css';  
 
-const Navbar = () => {
+const Navbar = ( {course_id }) => {
   return (
     <nav id="main-navbar" className={styles.navbar} aria-label="Общая навигация по сайту">
       <Logo />
--- frontend/src/pages/EditCoursePage.jsx
+++ frontend/src/pages/EditCoursePage.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Navbar from '../components/EditCourseComponents/Navbar';
+import Sidebar from '../components/EditCourseComponents/Sidebar';
+import styles from '../components/EditCourseComponents/Sidebar.module.css';
 
 import { useParams } from 'react-router-dom';
 
 const EditCoursePage = () => {
     const { id } = useParams();
+
     return (
-        <>
-            <Navbar/>
-        </>
-    )
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <Navbar course_id={id}/>
+            </div>
+            <div className={styles.mainContent}>
+                <Sidebar course_id={id} />
+                {/* <main className={styles.content}>
+                    <h1 align="center">Шаг 1 — Дескрипторы</h1>
+                </main> */}
+            </div>
+        </div>
+    );
 }
 
 export default EditCoursePage;
