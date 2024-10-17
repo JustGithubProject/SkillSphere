9432873236e9c6f1c668035f6ecdf29d9c61f164
JustGithubProject <Kropivadanil@gmail.com>
2024-10-17 19:57:01 +0300

    Improved Sidebar

---
+++ frontend/src/components/EditCourseComponents/ModuleForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import styles from './ModuleForm.module.css'; 
+
+const ModuleForm = ( { course_id }) => {
+    const [title, setTitle] = useState('');
+
+
+    const handleCreateModule = async (e) => {
+        e.preventDefault();
+        try {
+            const accessToken = Cookies.get('access_token');
+            const response = await axios.post(
+                'http://127.0.0.1:8000/api/v1/module',
+                {
+                    title: title,
+                    description: "TEMP VALUE FOR A WHILE", 
+                    course_id: course_id, 
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                }
+            );
+
+            if (response.status === 201) {
+                setTitle('');
+            }
+        } catch (error) {
+            console.log('Failed to create module:', error);
+        }
+    };
+
+    return (
+        <div className={styles.moduleFormContainerVeryUniqueClassThatWasCreatedByMe}>
+            <form onSubmit={handleCreateModule} className={styles.moduleForm}>
+                <div className={styles.formGroup}>
+                    <label htmlFor="title">Module Title</label>
+                    <input
+                        className={styles.classForInputCreatedByMe}
+                        type="text"
+                        id="title"
+                        placeholder="Enter Module Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className={styles.submitButton}>Create Module</button>
+            </form>
+        </div>
+    );
+};
+
+export default ModuleForm;
